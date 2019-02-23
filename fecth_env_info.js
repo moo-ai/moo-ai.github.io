295dc57af1d6a5ca0db0230e8d84ab77ea5c1f3a
wangxiyuan <wangxiyuan1007@gmail.com>
2019-02-23 16:35:36 +0800

    Add fecth env

---
+++ fecth_env_info.js
@@ -0,0 +1,15 @@
+onmessage = function (evt) {
+				var pr_commit_id = evt.pr_commit_id
+				var ip=''
+				while (true){
+					repo.contents("env_info/" + pr_commit_id + ".json").read()
+					var json_c_obj = JSON.parse(json_contents)
+					var ip_str = json_c_obj['ip']
+					ip = ip_str.split("\'")[1]
+					var build_id = json_c_obj['build_id']
+					if ip !='' {
+						break
+					}
+				}
+				postMessage(ip+'|'+build_id)
+			}
