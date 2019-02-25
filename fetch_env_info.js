e939528e4c9bb2854751eea88acc92db96144cf0
wangxiyuan <wangxiyuan1007@gmail.com>
2019-02-25 10:36:25 +0800

    debug env

--- fetch_env_info.js
+++ fetch_env_info.js
@@ -1,3 +1,13 @@
+function wait(sec) {
+    var now = new Date()
+    var exitTime = now.getTime() + sec
+    while (true) {
+        now = new Date();
+        if (now.getTime() > exitTime)
+            return
+    }
+}
+
 onmessage = function(evt) {
     const sleep = (milliseconds) => {
         return new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -7,24 +17,25 @@ onmessage = function(evt) {
     var build_id = ''
     var no_content = true
     var env_request
-    while (no_content) {
-        sleep(10000).then(() => {
-            env_request = new XMLHttpRequest()
-            env_request.onreadystatechange = function() {
-                if (env_request.readyState === 4) {
-                    if (env_request.status === 200) {
-                        var json_c_obj = JSON.parse(env_request.responseText)
-                        var ip_str = json_c_obj['ip']
-                        ip = ip_str.split("\'")[1]
-                        build_id = json_c_obj['build_id']
 
-                        no_content = false
-                        postMessage([ip, build_id])
-                    }
+    while (no_content) {
+        wait(10000)
+ 
+        env_request = new XMLHttpRequest()
+        env_request.onreadystatechange = function() {
+            if (env_request.readyState === 4) {
+                if (env_request.status === 200) {
+                    var json_c_obj = JSON.parse(env_request.responseText)
+                    var ip_str = json_c_obj['ip']
+                    ip = ip_str.split("\'")[1]
+                    build_id = json_c_obj['build_id']
+
+                    no_content = false
+                    postMessage([ip, build_id])
                 }
             }
-            env_request.open('GET', '/env_info/' + pr_commit_id + '.json', false)
-            env_request.send()
-        })
+        }
+        env_request.open('GET', '/env_info/' + pr_commit_id + '.json', false)
+        env_request.send()
     }
 }
