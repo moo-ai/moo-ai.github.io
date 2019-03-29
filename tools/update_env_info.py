8e2d379a3d2d05a9bb91b813778c588ea000c189
wangxiyuan <wangxiyuan1007@gmail.com>
2019-03-29 15:24:00 +0800

    Update tool

--- tools/update_env_info.py
+++ tools/update_env_info.py
@@ -1,54 +1,45 @@
 import argparse
 import github
 import json
-import wget
-
-DOWNLOAD_FILE = 'env.json'
 
 
-def download_env_file(keeping=False):
-    file = 'task_list.json'
-    if keeping:
-        file = 'task_list_sleep.json'
-    url = ("https://raw.githubusercontent.com/moo-ai/moo-ai.github.io/"
-           "master/env_info/%s" % file)
-    wget.download(url, DOWNLOAD_FILE)
+class EnvInfoUploader(object):
+    def __init__(self, user, password, job_name, patch_set, public_ip,
+                 result_url, keeping_env):
+        self.github_obj = github.Github(user, password)
+        self.repo = self.github_obj.get_user().get_repo('moo-ai.github.io')
+        self.job_name = job_name
+        self.patch_set = patch_set
+        self.public_ip = public_ip
+        self.result_url = result_url
+        self.keeping_env = keeping_env
+        self.remote_env_file = ('task_list_sleep.json' if keeping_env
+                                else 'task_list.json')
+        self.content = self.repo.get_contents(
+            'env_info/%s' % self.remote_env_file, ref="master")
+        self.content_json = json.loads(self.content.decoded_content)
 
+    def _update_env_info(self):
+        self.content_json[self.job_name] = {
+            "patch_set": self.patch_set,
+        }
+        if self.keeping_env:
+            self.content_json[self.job_name]["ip"] = self.public_ip
+            self.content_json[self.job_name]["credential"] = "demo/demo"
+        else:
+            self.content_json[self.job_name]["result_url"] = self.result_url
 
-def update_env_info(job_name, patch_set, ip, result_url, keeping=False):
-    with open(DOWNLOAD_FILE, 'r') as json_file:
-        exist_data = json.load(json_file)
+    def _refresh_env_info(self):
 
-    exist_data[job_name] = {
-        "patch_set": patch_set,
-    }
-    if keeping:
-        exist_data[job_name]["ip"] = ip
-        exist_data[job_name]["credential"] = "demo/demo"
-    else:
-        exist_data[job_name]["result_url"] = result_url
+        self.repo.update_file(self.content.path,
+                              "upload %s ci job" % args.job,
+                              json.dumps(self.content_json), self.content.sha,
+                              branch="master")
+        print("Success update %s" % self.remote_env_file)
 
-    with open(DOWNLOAD_FILE, 'w') as file:
-        json.dump(exist_data, file)
-
-
-def refresh_env_info(keeping=False):
-    with open(DOWNLOAD_FILE, 'r') as env_file:
-        cont = env_file.read()
-    github_obj = github.Github(args.user,
-                               args.password)
-    repo = github_obj.get_user().get_repo('moo-ai.github.io')
-    env_file_name = 'env_info/task_list.json'
-    if keeping:
-        env_file_name = 'env_info/task_list_sleep.json'
-    try:
-        contents = repo.get_contents(env_file_name, ref="master")
-    except github.UnknownObjectException:
-        exit(1)
-
-    repo.update_file(contents.path, "upload %s ci job" % args.job, cont,
-                     contents.sha, branch="master")
-    print("Success update %s" % env_file_name)
+    def upload_env_info(self):
+        self._update_env_info()
+        self._refresh_env_info()
 
 
 if __name__ == "__main__":
@@ -61,9 +52,9 @@ if __name__ == "__main__":
     parser.add_argument('--password', metavar='<password>', required=True,
                         help="Github user password")
     parser.add_argument('--job', metavar='<job>', required=True,
-                        help="The webhook server's IP")
+                        help="The moo job name")
     parser.add_argument('--patch', metavar='<patch>', required=True,
-                        help="The webhook server's IP")
+                        help="The moo pull request id")
     parser.add_argument('--ip', metavar='<ip>', required=True,
                         help="Environment IP")
     parser.add_argument('--url', metavar='<url>', required=True,
@@ -77,7 +68,7 @@ if __name__ == "__main__":
     else:
         keep_env = False
 
-    download_env_file(keep_env)
-    update_env_info(args.job, args.patch, args.ip, args.url,
-                    keeping=keep_env)
-    refresh_env_info(keeping=keep_env)
+    uploader = EnvInfoUploader(args.user, args.password, args.job, args.patch,
+                               args.ip, args.url, keep_env)
+
+    uploader.upload_env_info()
